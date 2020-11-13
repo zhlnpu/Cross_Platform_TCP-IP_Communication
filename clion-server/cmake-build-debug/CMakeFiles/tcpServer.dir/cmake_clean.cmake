439a9ac18ebdaea2daa7e73dc556387fdd7ab654
zhlnpu <zhlnpu@gmail.com>
2020-11-13 14:22:55 +1300

    add sample tcpip

---
+++ clion-server/cmake-build-debug/CMakeFiles/tcpServer.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcpServer.dir/main.cpp.o"
+  "tcpServer"
+  "tcpServer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
